7cdaaaa651aa2fedff9bb917e42770f756ea427f
Aleksa Milosevic <aleksa.milosevic807@gmail.com>
2026-07-09 17:50:02 +0200

    Izbrisan stari projekat

--- data-ingestion-service/Services/IMessagePublisher.cs
+++
@@ -1,7 +0,0 @@
-namespace DataIngestionService.Services;
-
-public interface IMessagePublisher
-{
-    Task PublishAsync(string payload, CancellationToken ct);
-    Task EnsureConnectedAsync(CancellationToken ct);
-}
